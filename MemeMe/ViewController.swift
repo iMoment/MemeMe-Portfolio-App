247c847f9398134b426648f518b06c3d3e59decf
Stanley Pan <stanleypan.egre@gmail.com>
2016-01-21 16:44:27 +0900

    Add protocols and delegate methods for UIImagePickerController

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     
@@ -22,9 +22,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Method for selecting an image from Photo library
+    // IBAction linked to 'Album' button in toolbar
     @IBAction func pickAnImage(sender: AnyObject) {
         let pickerController = UIImagePickerController()
+        pickerController.delegate = self
         self.presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
+    // Delegate method when a user selects an image or movie
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imagePickerView.image = selectedImage
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+    // Delegate method when a user selects cancel
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
 }
